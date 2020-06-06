11d1106654fb63b0bf314819290c7530643967d8
andrzej-malina <andrzej.malina@gmail.com>
2020-06-07 00:01:46 +0200

    Add files via upload

--- dataflow.py
+++ dataflow.py
@@ -14,12 +14,9 @@ def create_prediction_folders(number):
 
 def get_labels():
     names = sorted(['ok', 'kropla', 'kaktus', 'ksiezyc', 'plama', 'stop', 'klodka', 'cel', 'kotwica', 'serce', 'bomba',
-              'zebra',
-              'kostka_lodu', 'piesek', 'butelka', 'ser', 'konik', 'pajeczyna', 'marchewka', 'zegar', 'lisc', 'usta',
-              'jablko',
-              'okulary', 'oko', 'gwiazdka', 'dinozaur', 'auto', 'zarowka', 'sniezynka', 'yingyang', 'wykrzyknik',
-              'ognisko',
-              'ludek', 'stokrotka', 'klucz', 'klucz_muzyczny', 'doble', 'piorun', 'biedronka', 'duch', 'delfin', 'art',
+              'zebra', 'kostka_lodu', 'piesek', 'butelka', 'ser', 'konik', 'pajeczyna', 'marchewka', 'zegar', 'lisc', 'usta',
+              'jablko','okulary', 'oko', 'gwiazdka', 'dinozaur', 'auto', 'zarowka', 'sniezynka', 'yingyang', 'wykrzyknik',
+              'ognisko', 'ludek', 'stokrotka', 'klucz', 'klucz_muzyczny', 'doble', 'piorun', 'biedronka', 'duch', 'delfin', 'art',
               'czacha', 'olowek', 'pajak', 'smok', 'swieca', 'kotek', 'drzewo', 'balwan', 'pytajnik', 'balon', 'iglo',
               'koniczyna', 'klaun', 'nozyce'])
 
